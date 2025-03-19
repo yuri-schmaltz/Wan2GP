ac18ec6b4f930997f924c4cd5c71fab88857d231
Chris Malone <chrismrulz@hotmail.com>
2025-03-20 08:46:30 +1100

    added output path to allowed_paths to prevent gradio errors on external paths

--- gradio_server.py
+++ gradio_server.py
@@ -2122,6 +2122,6 @@ if __name__ == "__main__":
             url = "http://" + server_name 
         webbrowser.open(url + ":" + str(server_port), new = 0, autoraise = True)
 
-    demo.launch(server_name=server_name, server_port=server_port, share=args.share)
+    demo.launch(server_name=server_name, server_port=server_port, share=args.share, allowed_paths=[save_path])
 
  
