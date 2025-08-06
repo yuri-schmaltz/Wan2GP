3f20921e7a6ae06cc7baa50b04e8d2c5e77825d5
deepbeepmeep <deepbeepmeep@yahoo.com>
2025-08-06 20:40:08 +0200

    missing qwen

---
+++ models/qwen/qwen_handler.py
@@ -0,0 +1,78 @@
+import torch
+
+def get_qwen_text_encoder_filename(text_encoder_quantization):
+    text_encoder_filename = "ckpts/Qwen2.5-VL-7B-Instruct/Qwen2.5-VL-7B-Instruct_bf16.safetensors"
+    if text_encoder_quantization =="int8" and False:
+        text_encoder_filename = text_encoder_filename.replace("bf16", "quanto_bf16_int8") 
+    return text_encoder_filename
+
+class family_handler():
+    @staticmethod
+    def query_model_def(base_model_type, model_def):
+        model_def_output = {
+            "image_outputs" : True,
+            "no_negative_prompt" : True,
+        }
+
+        model_def_output["embedded_guidance"] = True
+
+        return model_def_output
+
+    @staticmethod
+    def query_supported_types():
+        return ["qwen_image_20B"]
+
+    @staticmethod
+    def query_family_maps():
+        return {}, {}
+
+    @staticmethod
+    def query_model_family():
+        return "qwen"
+
+    @staticmethod
+    def query_family_infos():
+        return {"qwen":(40, "Qwen")}
+
+    @staticmethod
+    def query_model_files(computeList, base_model_type, model_filename, text_encoder_quantization):
+        text_encoder_filename = get_qwen_text_encoder_filename(text_encoder_quantization)    
+        return  {  
+            "repoId" : "DeepBeepMeep/Qwen_image", 
+            "sourceFolderList" :  ["", "Qwen2.5-VL-7B-Instruct"],
+            "fileList" : [ ["qwen_vae.safetensors", "qwen_vae_config.json", "qwen_scheduler_config.json"], ["merges.txt", "tokenizer_config.json", "config.json", "vocab.json"] + computeList(text_encoder_filename)  ]
+            }
+
+    @staticmethod
+    def load_model(model_filename, model_type, base_model_type, model_def, quantizeTransformer = False, text_encoder_quantization = None, dtype = torch.bfloat16, VAE_dtype = torch.float32, mixed_precision_transformer = False, save_quantized = False):
+        from .qwen_main import model_factory
+        from mmgp import offload
+
+        pipe_processor = model_factory(
+            checkpoint_dir="ckpts",
+            model_filename=model_filename,
+            model_type = model_type, 
+            model_def = model_def,
+            base_model_type=base_model_type,
+            text_encoder_filename= get_qwen_text_encoder_filename(text_encoder_quantization),
+            quantizeTransformer = quantizeTransformer,
+            dtype = dtype,
+            VAE_dtype = VAE_dtype, 
+            mixed_precision_transformer = mixed_precision_transformer,
+            save_quantized = save_quantized
+        )
+
+        pipe = {"tokenizer" : pipe_processor.tokenizer, "transformer" : pipe_processor.transformer, "text_encoder" : pipe_processor.text_encoder, "vae" : pipe_processor.vae}
+
+        return pipe_processor, pipe
+
+    @staticmethod
+    def update_default_settings(base_model_type, model_def, ui_defaults):
+        ui_defaults.update({
+            "embedded_guidance":  4,
+        })            
+        if model_def.get("reference_image", False):
+            ui_defaults.update({
+                "video_prompt_type": "KI",
+            })
+
---
+++ models/qwen/qwen_main.py
@@ -0,0 +1,113 @@
+
+from mmgp import offload
+import inspect
+from typing import Any, Callable, Dict, List, Optional, Union
+
+import numpy as np
+import torch, json, os
+
+from diffusers.image_processor import VaeImageProcessor
+from .transformer_qwenimage import QwenImageTransformer2DModel
+
+from diffusers.utils import logging, replace_example_docstring
+from diffusers.utils.torch_utils import randn_tensor
+from transformers import Qwen2_5_VLForConditionalGeneration, Qwen2Tokenizer, AutoTokenizer
+from .autoencoder_kl_qwenimage import AutoencoderKLQwenImage
+from diffusers import FlowMatchEulerDiscreteScheduler
+from .pipeline_qwenimage import QwenImagePipeline
+
+class model_factory():
+    def __init__(
+        self,
+        checkpoint_dir,
+        model_filename = None,
+        model_type = None, 
+        model_def = None,
+        base_model_type = None,
+        text_encoder_filename = None,
+        quantizeTransformer = False,
+        save_quantized = False,
+        dtype = torch.bfloat16,
+        VAE_dtype = torch.float32,
+        mixed_precision_transformer = False
+    ):
+        
+        with open( os.path.join(checkpoint_dir, "qwen_scheduler_config.json"), 'r', encoding='utf-8') as f:
+            scheduler_config = json.load(f)
+        scheduler_config.pop("_class_name")
+        scheduler_config.pop("_diffusers_version")
+
+        scheduler = FlowMatchEulerDiscreteScheduler(**scheduler_config)
+
+        transformer_filename = model_filename[0]
+        tokenizer = AutoTokenizer.from_pretrained(os.path.join(checkpoint_dir,"Qwen2.5-VL-7B-Instruct")) 
+
+        with open("configs/qwen_image_20B.json", 'r', encoding='utf-8') as f:
+            transformer_config = json.load(f)
+        transformer_config.pop("_diffusers_version")
+        transformer_config.pop("_class_name")
+        transformer_config.pop("pooled_projection_dim")
+        
+        from accelerate import init_empty_weights
+        with init_empty_weights():
+            transformer = QwenImageTransformer2DModel(**transformer_config)
+        offload.load_model_data(transformer, transformer_filename)
+        # transformer = offload.fast_load_transformers_model("transformer_quanto.safetensors", writable_tensors= True , modelClass=QwenImageTransformer2DModel, defaultConfigPath="transformer_config.json")
+
+        text_encoder = offload.fast_load_transformers_model(text_encoder_filename,  writable_tensors= True , modelClass=Qwen2_5_VLForConditionalGeneration,  defaultConfigPath= os.path.join(checkpoint_dir, "Qwen2.5-VL-7B-Instruct", "config.json"))
+        # text_encoder = offload.fast_load_transformers_model(text_encoder_filename, do_quantize=True,  writable_tensors= True , modelClass=Qwen2_5_VLForConditionalGeneration, defaultConfigPath="text_encoder_config.json", verboseLevel=2)
+        # text_encoder.to(torch.float16)
+        # offload.save_model(text_encoder, "text_encoder_quanto_fp16.safetensors", do_quantize= True)
+
+        vae = offload.fast_load_transformers_model( os.path.join(checkpoint_dir,"qwen_vae.safetensors"), writable_tensors= True , modelClass=AutoencoderKLQwenImage, defaultConfigPath=os.path.join(checkpoint_dir,"qwen_vae_config.json"))
+        
+        self.pipeline = QwenImagePipeline(vae, text_encoder, tokenizer, transformer, scheduler)
+        self.vae=vae
+        self.text_encoder=text_encoder
+        self.tokenizer=tokenizer
+        self.transformer=transformer
+        self.scheduler=scheduler
+
+
+    def generate(
+        self,
+        seed: int | None = None,
+        input_prompt: str = "replace the logo with the text 'Black Forest Labs'",
+        sampling_steps: int = 20,
+        input_ref_images = None,
+        width= 832,
+        height=480,
+        embedded_guidance_scale: float = 4,
+        fit_into_canvas = None,
+        callback = None,
+        loras_slists = None,
+        batch_size = 1,
+        video_prompt_type = "",
+        **bbargs
+    ):
+        # Generate with different aspect ratios
+        aspect_ratios = {
+        "1:1": (1328, 1328),
+        "16:9": (1664, 928),
+        "9:16": (928, 1664),
+        "4:3": (1472, 1140),
+        "3:4": (1140, 1472)
+        }
+
+        # width, height = aspect_ratios["16:9"]
+
+        image = self.pipeline(
+        prompt=input_prompt,
+        width=width,
+        height=height,
+        num_inference_steps=sampling_steps,
+        num_images_per_prompt = batch_size,
+        true_cfg_scale=embedded_guidance_scale,
+        callback = callback,
+        pipeline=self,
+        loras_slists=loras_slists,
+        generator=torch.Generator(device="cuda").manual_seed(seed)
+        )        
+        if image is None: return None
+        return image.transpose(0, 1)
+
