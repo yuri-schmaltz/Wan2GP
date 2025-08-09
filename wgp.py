b9921bddf8dda57a80cf0c529133c35a7cee47a1
deepbeepmeep <deepbeepmeep@yahoo.com>
2025-08-09 20:21:54 +0200

    updated version

--- wgp.py
+++ wgp.py
@@ -50,7 +50,7 @@ AUTOSAVE_FILENAME = "queue.zip"
 PROMPT_VARS_MAX = 10
 
 target_mmgp_version = "3.5.7"
-WanGP_version = "7.73"
+WanGP_version = "7.74"
 settings_version = 2.23
 max_source_video_frames = 3000
 prompt_enhancer_image_caption_model, prompt_enhancer_image_caption_processor, prompt_enhancer_llm_model, prompt_enhancer_llm_tokenizer = None, None, None, None
